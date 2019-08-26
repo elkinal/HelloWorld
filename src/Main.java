506de05f9b31d70e4d3e5dd128147995ab6deb6d
admin <a@a.com>
2019-08-26 14:30:25 +0300

    This is the second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World V1");
+        System.out.println("Hello World V1.1");
+        System.out.println(newFunction());
+    }
+    private static String newFunction() {
+        return "Testing";
     }
 }
