693b0dc0aec897152fa45ae222591375fdae3576
admin <a@a.com>
2019-08-26 14:25:40 +0300

    Wait for the user input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World V1");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
